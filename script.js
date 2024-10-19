696edbdfed1a036abd4c444e4b29a2319134a88f
dzoniss1 <128727990+dzoniss1@users.noreply.github.com>
2024-10-19 18:41:25 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -55,3 +55,28 @@ function del(){
     currentInput = result.value;
 }
 
+
+/* 
+  DARK MODE LIGHT MODE 
+                      */
+
+  function DarkMode() {
+    const elementsToToggle = [
+        { selector: '.function', className: 'dark-mode-operators' },
+        { selector: '.number', className: 'dark-mode-number' },
+        { selector: '.toprow', className: 'dark-mode-toprow' },
+        { element: document.getElementById('result'), className: 'dark-mode' },
+        { element: document.getElementById('calculation'), className: 'dark-mode' },
+        { element: document.querySelector('.iphone'), className: 'dark-mode' },
+        { element: document.getElementById('buttons'), className: 'dark-mode' }
+    ];
+
+    elementsToToggle.forEach(item => {
+        if (item.selector) {
+            const buttons = document.querySelectorAll(item.selector);
+            buttons.forEach(button => button.classList.toggle(item.className));
+        } else if (item.element) {
+            item.element.classList.toggle(item.className);
+        }
+    });
+}
